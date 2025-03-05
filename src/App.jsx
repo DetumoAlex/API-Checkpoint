98b67c8f72063704a9e2f498602835fba42a03fa
Ehis <chrisehist@gmail.com>
2025-03-05 14:19:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UserList from "./components /UsersList"; 
+const App = () => {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
